651d84fdc57a4313e31bd931e5f49466e8d5a78b
Khansa Shakeel <khansashakeel24@gmail.com>
2026-08-26 17:45:54 +0500

    Revert hero motion experiments

--- rewrite.sh
+++
@@ -1,6 +0,0 @@
-git filter-branch --env-filter '
-export GIT_AUTHOR_NAME="Khansa Shakeel"
-export GIT_AUTHOR_EMAIL="khansashakeel24@gmail.com"
-export GIT_COMMITTER_NAME="Khansa Shakeel"
-export GIT_COMMITTER_EMAIL="khansashakeel24@gmail.com"
-' --tag-name-filter cat -- --branches --tags
